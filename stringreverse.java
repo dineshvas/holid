0f8b1283eab27854e7b686ae7916dd3455825486
dineshvas <dineshkumar9569@gmail.com>
2017-06-19 17:09:40 +0530

    Add files via upload

---
+++ stringreverse.java
@@ -0,0 +1,11 @@
+package guvi;
+
+public class stringreverse {
+	public static void main(String[] args){
+		String str="hello";
+		StringBuffer sb=new StringBuffer(str);
+		sb.reverse();
+		System.out.println("Reverse strinig is:"+sb);		
+	}
+
+}
